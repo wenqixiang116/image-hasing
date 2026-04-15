4ed4f23a8e4237fee6ca3c3b17b92ac9f00b6bca
wenqixiang116 <176203508+wenqixiang116@users.noreply.github.com>
2026-04-15 14:04:51 +0000

    Add new image hashing algorithms: dhash_vertical and marr_hildreth_hash

--- tests/test_new_algorithms.py
+++ tests/test_new_algorithms.py
@@ -1,6 +1,6 @@
 import unittest
 from PIL import Image
-from image_hashing import whash, colorhash
+from image_hashing import whash, colorhash, dhash_vertical, marr_hildreth_hash
 
 class TestNewAlgorithms(unittest.TestCase):
     def setUp(self):
@@ -36,6 +36,34 @@ class TestNewAlgorithms(unittest.TestCase):
 
         # Test diff
         h3 = colorhash(self.image2)
+        self.assertNotEqual(h, h3)
+
+    def test_dhash_vertical(self):
+        # Create an image with vertical variation
+        img = Image.new('RGB', (100, 100), color='black')
+        for y in range(50, 100):
+            for x in range(100):
+                img.putpixel((x, y), (255, 255, 255))
+
+        h = dhash_vertical(img)
+        self.assertIsInstance(h, str)
+        self.assertEqual(len(h), 16)
+
+        h2 = dhash_vertical(img)
+        self.assertEqual(h, h2)
+
+        h3 = dhash_vertical(self.image2)
+        self.assertNotEqual(h, h3)
+
+    def test_marr_hildreth_hash(self):
+        h = marr_hildreth_hash(self.image)
+        self.assertIsInstance(h, str)
+        self.assertEqual(len(h), 16)
+
+        h2 = marr_hildreth_hash(self.image)
+        self.assertEqual(h, h2)
+
+        h3 = marr_hildreth_hash(self.image2)
         self.assertNotEqual(h, h3)
 
 if __name__ == '__main__':
