473d3e496acd7037e6dcc770d164f1e8c97ef98f
wenqixiang116 <176203508+wenqixiang116@users.noreply.github.com>
2026-06-20 12:56:37 +0000

    Add Vertical Difference Hash and Marr-Hildreth Hash algorithms

--- image_hashing/algorithms.py
+++ image_hashing/algorithms.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import scipy.fftpack
+import scipy.ndimage
 import pywt
 
 def average_hash(image, hash_size=8):
@@ -36,6 +37,23 @@ def dhash(image, hash_size=8):
     pixels = np.asarray(image)
     # Compare pixel[x, y] to pixel[x+1, y]
     diff = pixels[:, 1:] > pixels[:, :-1]
+
+    return _binary_array_to_hex(diff.flatten())
+
+def dhash_vertical(image, hash_size=8):
+    """
+    Vertical Difference Hash computation.
+    """
+    # Resize to hash_size x (hash_size + 1)
+    image = image.resize((hash_size, hash_size + 1), Image.Resampling.LANCZOS)
+
+    # Grayscale
+    image = image.convert("L")
+
+    # Compute differences
+    pixels = np.asarray(image)
+    # Compare pixel[y, x] to pixel[y+1, x]
+    diff = pixels[1:, :] > pixels[:-1, :]
 
     return _binary_array_to_hex(diff.flatten())
 
@@ -90,6 +108,30 @@ def whash(image, hash_size=8):
 
     # Compute bits
     diff = LL > med
+
+    return _binary_array_to_hex(diff.flatten())
+
+def marr_hildreth_hash(image, hash_size=8, scale=4, sigma=2.5):
+    """
+    Marr-Hildreth Hash computation.
+    """
+    image_size = hash_size * scale
+    # Resize
+    image = image.resize((image_size, image_size), Image.Resampling.LANCZOS)
+
+    # Grayscale
+    image = image.convert("L")
+
+    pixels = np.asarray(image).astype(float)
+
+    # Apply Laplacian of Gaussian
+    blocks = scipy.ndimage.gaussian_laplace(pixels, sigma=sigma)
+
+    # Subsample
+    blocks_sub = blocks[::scale, ::scale]
+
+    # Compute bits
+    diff = blocks_sub < 0
 
     return _binary_array_to_hex(diff.flatten())
 
